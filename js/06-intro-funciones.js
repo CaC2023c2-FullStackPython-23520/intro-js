b0063218315d66b1ac2737fc02536696b9e039db
Charly Cimino <carlosecimino@gmail.com>
2023-10-02 15:39:52 -0300

    Funciones

---
+++ js/06-intro-funciones.js
@@ -0,0 +1,16 @@
+// Definición de funciones
+
+function siguiente (n) {
+    return n + 1
+}
+
+function doble (n) {
+    return n * 2;
+}
+
+
+// Programa principal
+let resultado = doble(siguiente(4));
+console.log(resultado);
+
+
